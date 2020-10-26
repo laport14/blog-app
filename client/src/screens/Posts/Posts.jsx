a9850992497dbfb57bf748ac7298de7edf764c56
Sutton <suttondemlong@gmail.com>
2020-10-26 15:03:44 -0400

    Begin Posts.jsx

--- client/src/screens/Posts/Posts.jsx
+++ client/src/screens/Posts/Posts.jsx
@@ -1,4 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import '.Posts.css'
+
+import Product from '../../components/Product/Product'
+import Layout from '../../components/shared/Layout/Layout'
+import { getProducts } from '../../services/products'
+
+const Posts = (props) => {
+  const [allPosts, setAllPosts] = useState([])
+  const [queriedPosts, setQueriedPosts] = useState([])
+  const [sortType, setSortType] = useState([])
+
+  useEffect()
+}
 
 function Posts(props) {
   return (
