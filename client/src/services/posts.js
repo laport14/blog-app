3a9e3c4d57d6b74d862e83df1422d651eb955832
Geoffrey LaPorte <geoffreylaporte@Geoffreys-Air.attlocal.net>
2020-10-26 13:08:02 -0500

    Front End Layout Complete

--- client/src/services/posts.js
+++ client/src/services/posts.js
@@ -1,1 +1,46 @@
-import api from './apiConfig';
+import api from './apiConfig'
+
+export const getPosts = async () => {
+  try {
+      const response = await api.get('/posts')
+      return response.data
+  } catch (error) {
+      throw error
+  }
+}
+
+export const getProduct = async id => {
+  try {
+      const response = await api.get(`/posts/${id}`)
+      return response.data
+  } catch (error) {
+      throw error
+  }
+}
+
+export const createProduct = async product => {
+  try {
+      const response = await api.post('/posts', product)
+      return response.data
+  } catch (error) {
+      throw error
+  }
+}
+
+export const updateProduct = async (id, product) => {
+  try {
+      const response = await api.put(`/posts/${id}`, product)
+      return response.data
+  } catch (error) {
+      throw error
+  }
+}
+
+export const deleteProduct = async id => {
+  try {
+      const response = await api.delete(`/posts/${id}`)
+      return response.data
+  } catch (error) {
+      throw error
+  }
+}
