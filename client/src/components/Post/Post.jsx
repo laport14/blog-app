b50a61ed9d22f3c6aed04a7cd0bd405c3728bf75
Geoff <geoffreylaporte@gmail.com>
2020-10-26 16:51:08 -0500

    MVP met

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 const Post = (props) => {
     return (
         <>
-            <Link className="post" to={`/products/${props._id}`}>
+            <Link className="post" to={`/posts/${props._id}`}>
                 <img className="post-image" src={props.imgURL} alt={props.name} />
                 <div className="post-title">{props.title}</div>
                 <div className="author">{`${props.author}`}</div>
