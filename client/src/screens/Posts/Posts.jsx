9e81beb5e763bfef6d374eccddb2321af1a00b3f
Geoff <geoffreylaporte@gmail.com>
2020-10-26 14:33:26 -0500

    Post screen Complete

--- client/src/screens/Posts/Posts.jsx
+++ client/src/screens/Posts/Posts.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import "./Posts.css";
 
-function Posts(props) {
+import Post from "../../components/Post/Post";
+import Layout from "../../components/shared/Layout/Layout";
+import { getPosts } from "../../services/posts";
+
+const Posts = (props) => {
+  const [queriedPosts, setQueriedPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const posts = await getPosts();
+      setQueriedPosts(posts);
+    };
+    fetchPosts();
+  }, []);
+
+  const postsJSX = queriedPosts.map((post, index) => (
+    <Post
+      _id={post._id}
+      title={post.title}
+      imgURL={post.imgURL}
+      content={post.content}
+      author={post.author}
+      key={index}
+    />
+  ));
+
   return (
-    <div>
-      
-    </div>
+    <Layout>
+      <div className="posts">{postsJSX}</div>
+    </Layout>
   );
-}
+};
 
 export default Posts;
